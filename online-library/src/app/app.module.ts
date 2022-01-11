295ac47a2708b00d6fee36a8fb7542fcadd5b199
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-01-11 09:41:50 +0530

    AuthModule

--- online-library/src/app/app.module.ts
+++ online-library/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   // Registration of components
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   // Using other modules.
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   // Provide services
   providers: [],
